fdc4e863dc31d9ab8162671b36893efca28afdb5
김영훈 <hunifive@naver.com>
2025-07-17 23:11:41 +0900

    refactory(lv4): 추후 개발을 위해 사용자 입력에 따라 메뉴를 추가할 수 있도록 리팩토링
    1. `Menu` 클래스에 `add` 메소드를 통해 `MenuItem` 을 추가할 수 있도록
    2. `MenuItem` 의 `price` 속성을 `String` 으로 받고 생성자에서 `BigDecimal` 로 변환하도록

--- src/main/java/com/example/lv4/Main.java
+++ src/main/java/com/example/lv4/Main.java
@@ -1,33 +1,26 @@
 package com.example.lv4;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Menu 객체 생성을 통해 이름 설정
-        List<MenuItem> burgerMenuItems = new ArrayList<>();
-        burgerMenuItems.add(new MenuItem("ShackBurger", new BigDecimal("6.9"), "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        burgerMenuItems.add(new MenuItem("SmokeShack", new BigDecimal("18.992900"), "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        burgerMenuItems.add(new MenuItem("Cheeseburger", new BigDecimal("7.9231231"), "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        burgerMenuItems.add(new MenuItem("Hamburger", new BigDecimal("5.4430"), "비프패티를 기반으로 야채가 들어간 기본버거"));
+        Menu burgerMenu = new Menu("Burger Menu");
+        burgerMenu.add(new MenuItem("ShackBurger", "6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgerMenu.add(new MenuItem("SmokeShack", "18.992900", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgerMenu.add(new MenuItem("Cheeseburger", "7.9231231", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgerMenu.add(new MenuItem("Hamburger", "5.4430", "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        List<MenuItem> drinkMenuItems = new ArrayList<>();
-        drinkMenuItems.add(new MenuItem("Coke", new BigDecimal("3.23"), "코카콜라"));
-        drinkMenuItems.add(new MenuItem("Milk", new BigDecimal("2.5"), "우유"));
-        drinkMenuItems.add(new MenuItem("Water", new BigDecimal("1.1"), "시원한 물"));
+        Menu drinkMenu = new Menu("Drinks Menu");
+        drinkMenu.add(new MenuItem("Coke", "3.23", "코카콜라"));
+        drinkMenu.add(new MenuItem("Milk", "2.5", "우유"));
+        drinkMenu.add(new MenuItem("Water", "1.1", "시원한 물"));
 
-        List<MenuItem> dessertMenuItems = new ArrayList<>();
-        dessertMenuItems.add(new MenuItem("Cookie", new BigDecimal("2.4"), "쿠키"));
-        dessertMenuItems.add(new MenuItem("French Fries", new BigDecimal("1.812"), "감자튀김"));
-        dessertMenuItems.add(new MenuItem("Fried Chicken", new BigDecimal("9.99"), "후라이드 치킨"));
-
-        // Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입
-        Menu burgerMenu = new Menu("Burgers", burgerMenuItems);
-        Menu drinkMenu = new Menu("Drinks", drinkMenuItems);
-        Menu dessertMenu = new Menu("Desserts", dessertMenuItems);
+        Menu dessertMenu = new Menu("Dessert Menu");
+        dessertMenu.add(new MenuItem("Cookie", "2.4", "쿠키"));
+        dessertMenu.add(new MenuItem("French Fries", "1.812", "감자튀김"));
+        dessertMenu.add(new MenuItem("Fried Chicken", "9.99", "후라이드 치킨"));
 
         List<Menu> menus = new ArrayList<>(Arrays.asList(burgerMenu, drinkMenu, dessertMenu));
 
