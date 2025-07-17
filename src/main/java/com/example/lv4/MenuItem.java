a7c988b89bcbbfaf7abdc09f56f9c236d189cbdf
김영훈 <hunifive@naver.com>
2025-07-17 19:13:41 +0900

    feat(lv4): `MenuItem` 을 `Menu` 클래스에서 관리하도록 분리

--- src/main/java/com/example/lv4/MenuItem.java
+++ src/main/java/com/example/lv4/MenuItem.java
@@ -1,30 +1,21 @@
 package com.example.lv4;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 public class MenuItem {
-    private static int count = 0;
-    private final int num;
+    // 이름, 가격, 설명 필드 선언하여 관리
     private final String name;
     private final BigDecimal price;
     private final String description;
 
+    // 생성자
     public MenuItem(String name, BigDecimal price, String description) {
-        this.num = ++count;
         this.name = name;
         this.price = price;
         this.description = description;
     }
 
-    public void printInfo() {
-        System.out.printf("%d. %-15s | W %10s | %s %n", getNum(), getName(), getPrice(), getDescription());
-    }
-
-    public int getNum() {
-        return num;
-    }
-
+    // 구조에 맞게 함수를 선언해놓고 가져다 사용하세요.
     public String getName() {
         return name;
     }
@@ -35,12 +26,5 @@ public class MenuItem {
 
     public String getDescription() {
         return description;
-    }
-
-    public static MenuItem fromNum(List<MenuItem> menuItems, int num) throws IllegalArgumentException {
-        return menuItems.stream()
-                .filter(menuItem -> menuItem.getNum() == num)
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("선택하신 메뉴는 존재하지 않습니다."));
     }
 }
