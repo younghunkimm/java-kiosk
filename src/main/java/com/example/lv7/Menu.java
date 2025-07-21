af42e881b541e3924315485ec3fb6a1aeb36893b
김영훈 <hunifive@gmail.com>
2025-07-21 11:20:29 +0900

    chore(lv7): lv6 복사

---
+++ src/main/java/com/example/lv7/Menu.java
@@ -0,0 +1,40 @@
+package com.example.lv7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    // 카테고리
+    private final String category;
+
+    // MenuItem 클래스를 List로 관리
+    private final List<MenuItem> menuItems = new ArrayList<>();
+
+    // 생성자
+    public Menu(String category) {
+        this.category = category;
+    }
+
+    // List에 들어있는 MenuItem을 순차적으로 보여주는 함수
+    public void printMenuItemAll() {
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem menuItem = menuItems.get(i);
+            System.out.printf("%d. %s %n", i + 1, menuItem.toString());
+        }
+    }
+
+    // List를 리턴하는 함수
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    // Category를 리턴하는 함수
+    public String getCategory() {
+        return category;
+    }
+
+    // MenuItem을 배열에 추가
+    public void add(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+}
