19646f7fa325d8db21f12df419afa22e52a911bd
김영훈 <hunifive@gmail.com>
2025-07-23 13:46:50 +0900

    refactor(lv7): `start()` 메서드 내부의 로직을 메서드로 분리

--- src/main/java/com/example/lv7/Kiosk.java
+++ src/main/java/com/example/lv7/Kiosk.java
@@ -32,103 +32,10 @@ public class Kiosk<T extends Number> {
                     break;
                 }
 
-                // 장바구니에 담긴 메뉴가 있다면
-                if (!cart.getCartList().isEmpty()) {
-                    // 조회
-                    // 입력값 == [메뉴종류의 개수 + 1]
-                    if (choiceCategory == menus.size() + 1) {
-                        System.out.printf("%n아래와 같이 주문 하시겠습니까?%n");
+                if (printCartMenu(choiceCategory)) continue;
+                if (printOrderCancel(choiceCategory)) continue;
 
-                        System.out.printf("%n[ Orders ]%n");
-
-                        // 장바구니에 있는 메뉴들을 출력한다.
-                        for (Map.Entry<MenuItem<T>, Integer> entry : cart.getCartList().entrySet()) {
-                            System.out.printf("🥄 %s (%d개) %n", entry.getKey().toString(), entry.getValue());
-                        }
-
-                        // 장바구니에 있는 메뉴들의 가격 합계를 출력한다.
-                        System.out.printf("%n[ Total ]%n");
-                        System.out.printf("💰 W %s %n%n", cart.getTotalPrice());
-
-                        System.out.println("1. 주문       2. 메뉴판       3. 제거");
-                        String choiceOrder = scanner.next();
-                        if ("1".equals(choiceOrder)) {
-                            // 할인 정보 출력
-                            System.out.println("\n할인 정보를 입력해주세요.\n");
-                            for (UserType userType : UserType.values()) {
-                                System.out.printf("%d. %-15s : %s%%%n", userType.ordinal() + 1, userType.getName(), userType.getDiscount().multiply(BigDecimal.valueOf(100)).stripTrailingZeros().toPlainString());
-                            }
-
-                            UserType selectedUserType = UserType.fromOrdinal(scanner.nextInt());
-                            BigDecimal calcPrice = selectedUserType.getDiscountPrice(cart.getTotalPrice());
-
-                            System.out.printf("%n👏 주문이 완료되었습니다. 금액은 W %s 입니다.%n", calcPrice.toPlainString());
-                            cart.clear();
-                        }
-                        else if ("3".equals(choiceOrder)) {
-                            // 장바구니 메뉴 제거
-                            // Map은 index를 지원하지 않는다.
-                            Set<MenuItem<T>> cartListKeys = cart.getCartList().keySet();
-                            // 키값들을 List 배열로 설정
-                            List<MenuItem<T>> keyList = new ArrayList<>(cartListKeys);
-
-                            System.out.println("\n제거할 메뉴를 선택해주세요.\n");
-                            for (int i = 0; i < keyList.size(); i++) {
-                                MenuItem<T> key = keyList.get(i);
-                                Integer value = cart.getCartList().get(key);
-                                System.out.printf("%d. %s (%d개) %n", i + 1, key.toString(), value);
-                            }
-                            System.out.println("0. 취소");
-
-                            int choiceRemove = scanner.nextInt();
-                            MenuItem<T> removeItem = keyList.get(choiceRemove - 1);
-                            String removeItemName = removeItem.getName();
-                            cart.remove(removeItem);
-
-                            System.out.printf("%n❌ %s 메뉴가 장바구니에서 삭제 되었습니다.%n", removeItemName);
-                        }
-
-                        continue;
-                    }
-
-                    // 주문 취소
-                    // 입력값 == [메뉴종류의 개수 + 2]
-                    if (choiceCategory == menus.size() + 2) {
-                        System.out.printf("%n진행중인 주문을 취소하였습니다.%n");
-                        cart.clear();
-
-                        continue;
-                    }
-                }
-
-                // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
-                Menu<T> selectedMainMenu = menus.get(choiceCategory - 1);
-
-                // 메뉴 아이템 선택 안내
-                printMenuItemList(selectedMainMenu);
-
-                // 숫자 입력 받기
-                int choiceMenuItem = scanner.nextInt();
-                if (choiceMenuItem == 0) {
-                    System.out.println("뒤로가기를 선택하셨습니다.");
-                    continue;
-                }
-
-                // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
-                MenuItem<T> selectedMenuItem = selectedMainMenu.getMenuItems().get(choiceMenuItem - 1);
-                System.out.printf("✅ %s %n", selectedMenuItem.toString());
-
-                // 장바구니 추가 여부 안내
-                System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
-                System.out.println("1. 확인       2. 취소");
-
-                if ("1".equals(scanner.next())) {
-                    // 장바구니에 추가
-                    cart.put(selectedMenuItem);
-
-                    System.out.printf("%n🛒 %s 이 장바구니에 추가되었습니다.%n", selectedMenuItem.getName());
-                    System.out.printf("%n아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.%n");
-                }
+                printMenuSelection(choiceCategory);
             } catch (IndexOutOfBoundsException | InputMismatchException e) {
                 System.out.println("해당 메뉴는 존재하지 않습니다.");
             } catch (IllegalArgumentException e) {
@@ -154,6 +61,117 @@ public class Kiosk<T extends Number> {
             System.out.printf("%n[ ORDER MENU ]%n");
             System.out.printf("%d. %-10s | %s %n", menus.size() + 1, "Orders", "장바구니를 확인 후 주문합니다.");
             System.out.printf("%d. %-10s | %s %n", menus.size() + 2, "Cancel", "진행중인 주문을 취소합니다.");
+        }
+    }
+
+    private boolean printCartMenu(int choiceCategory) {
+        // 조회
+        // 장바구니에 담긴 메뉴가 있고, 입력값 == [메뉴종류의 개수 + 1]
+        if (!cart.getCartList().isEmpty() && choiceCategory == menus.size() + 1) {
+            System.out.printf("%n아래와 같이 주문 하시겠습니까?%n");
+
+            System.out.printf("%n[ Orders ]%n");
+
+            // 장바구니에 있는 메뉴들을 출력한다.
+            for (Map.Entry<MenuItem<T>, Integer> entry : cart.getCartList().entrySet()) {
+                System.out.printf("🥄 %s (%d개) %n", entry.getKey().toString(), entry.getValue());
+            }
+
+            // 장바구니에 있는 메뉴들의 가격 합계를 출력한다.
+            System.out.printf("%n[ Total ]%n");
+            System.out.printf("💰 W %s %n%n", cart.getTotalPrice());
+
+            System.out.println("1. 주문       2. 메뉴판       3. 제거");
+            String choiceOrder = scanner.next();
+
+            switch (choiceOrder) {
+                case "1" -> printOrderProcess();
+                case "3" -> removeCartItem();
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean printOrderCancel(int choiceCategory) {
+        if (!cart.getCartList().isEmpty() && choiceCategory == menus.size() + 2) {
+            System.out.printf("%n진행중인 주문을 취소하였습니다.%n");
+            cart.clear();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // 할인 정보 출력
+    private void printOrderProcess() {
+        System.out.println("\n할인 정보를 입력해주세요.\n");
+        for (UserType userType : UserType.values()) {
+            System.out.printf("%d. %-15s : %s%%%n", userType.ordinal() + 1, userType.getName(), userType.getDiscount().multiply(BigDecimal.valueOf(100)).stripTrailingZeros().toPlainString());
+        }
+
+        UserType selectedUserType = UserType.fromOrdinal(scanner.nextInt());
+        BigDecimal calcPrice = selectedUserType.getDiscountPrice(cart.getTotalPrice());
+
+        System.out.printf("%n👏 주문이 완료되었습니다. 금액은 W %s 입니다.%n", calcPrice.toPlainString());
+        cart.clear();
+    }
+
+    // 장바구니 메뉴 제거
+    private void removeCartItem() {
+        // Map은 index를 지원하지 않는다.
+        Set<MenuItem<T>> cartListKeys = cart.getCartList().keySet();
+        // 키값들을 List 배열로 설정
+        List<MenuItem<T>> keyList = new ArrayList<>(cartListKeys);
+
+        System.out.println("\n제거할 메뉴를 선택해주세요.\n");
+        for (int i = 0; i < keyList.size(); i++) {
+            MenuItem<T> key = keyList.get(i);
+            Integer value = cart.getCartList().get(key);
+            System.out.printf("%d. %s (%d개) %n", i + 1, key.toString(), value);
+        }
+        System.out.println("0. 취소");
+
+        int choiceRemove = scanner.nextInt();
+        MenuItem<T> removeItem = keyList.get(choiceRemove - 1);
+        String removeItemName = removeItem.getName();
+        cart.remove(removeItem);
+
+        System.out.printf("%n❌ %s 메뉴가 장바구니에서 삭제 되었습니다.%n", removeItemName);
+    }
+
+    private void printMenuSelection(int choiceCategory) {
+        // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+        Menu<T> selectedMainMenu = menus.get(choiceCategory - 1);
+
+        // 메뉴 아이템 선택 안내
+        printMenuItemList(selectedMainMenu);
+
+        // 숫자 입력 받기
+        int choiceMenuItem = scanner.nextInt();
+        if (choiceMenuItem == 0) {
+            System.out.println("뒤로가기를 선택하셨습니다.");
+
+            return;
+        }
+
+        // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
+        MenuItem<T> selectedMenuItem = selectedMainMenu.getMenuItems().get(choiceMenuItem - 1);
+        System.out.printf("✅ %s %n", selectedMenuItem.toString());
+
+        // 장바구니 추가 여부 안내
+        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.println("1. 확인       2. 취소");
+
+        if ("1".equals(scanner.next())) {
+            // 장바구니에 추가
+            cart.put(selectedMenuItem);
+
+            System.out.printf("%n🛒 %s 이 장바구니에 추가되었습니다.%n", selectedMenuItem.getName());
+            System.out.printf("%n아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.%n");
         }
     }
 
